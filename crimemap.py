23e244ecffdc91725a9195d030a243fcf900fbcc
Jorge-Monge <gis@jorgemonge.ca>
2017-04-25 17:22:31 -0600

    Correcting syntactic error

--- crimemap.py
+++ crimemap.py
@@ -15,18 +15,11 @@ def home():
         data = None
     return render_template("home.html", data=data)
 
-@app.route("/add", methods=["POSTS"])
+@app.route("/add", methods=["POST"])
 def add():
     try:
         data = request.form.get("userinput")
         DB.add_input(data)
-    except Exception as e:
-        print e
-    return home()
-
-def clear():
-    try:
-        DB.clear_all()
     except Exception as e:
         print e
     return home()
